9cfa3bd6870bd722b4f5cf1586af48647188ab17
Prateek <prateekbanra7@gmail.com>
2025-08-29 18:23:13 +0530

    first commit

---
+++ SB-mono-flux-App/src/main/java/in/abc/SbMonoFluxAppApplication.java
@@ -0,0 +1,13 @@
+package in.abc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMonoFluxAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMonoFluxAppApplication.class, args);
+	}
+
+}
