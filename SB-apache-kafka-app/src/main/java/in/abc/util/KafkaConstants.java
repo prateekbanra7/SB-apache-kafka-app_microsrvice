f6f66df0a9b705ee124ccda3902b105c996e10df
Prateek <prateekbanra7@gmail.com>
2025-08-29 18:17:24 +0530

    first commit

---
+++ SB-apache-kafka-app/src/main/java/in/abc/util/KafkaConstants.java
@@ -0,0 +1,15 @@
+package in.abc.util;
+
+/**
+ * This class is used to declare constants of this application
+ * 
+ * @author Ashok
+ *
+ */
+public class KafkaConstants {
+
+	public static final String TOPIC = "demo-abc-topic";
+	public static final String GROUP_ID = "group_customer";
+	public static final String HOST = "localhost:9092";
+
+}
